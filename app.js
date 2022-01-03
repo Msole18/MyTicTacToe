fe736fbed1ab14a53f52c21e4a8d0e764f5204d1
Msole18 <miguel.sole18@gmail.com>
2022-01-02 21:22:54 -0300

    css modal position ok, colores puede variar

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ const tictacControler = ( () => {
             activePlayer >= 0.5 ? activePlayer = 1 : activePlayer = 0;
             console.log('ramdom es: ' + activePlayer);
             return activePlayer;
+        },
+        animalPlayer: (type) => {
+            // e.target.classList.contains('modal-monkey') ?  player = 'monkey' : player = 'squirrel';
+            // console.log(`player select: ${player}`);
+
         }
     }
 
@@ -34,18 +39,13 @@ const UIController = ( () => {
         playerOne:'.player-0',
         playerTwo: '.player-1',
         modal: '.modal',
+        modalMsg: '.modal-msg',
+        modalFirstPlayer: '.first-player',
+        modalMonkey: '.modal-monkey',
+        modalSquirrel: '.modal-squirrel',
         box: '.box',
         boxImg: '.image'
-        // box0: '.box-0',
-        // box1: '.box-1', 
-        // box2: '.box-2',
-        // box3: '.box-3',
-        // box4: '.box-4',
-        // box5: '.box-5',
-        // box6: '.box-6',
-        // box7: '.box-7',
-        // box8: '.box-8'
-
+       
     }
 
     return {
@@ -69,17 +69,26 @@ const UIController = ( () => {
             //         document.querySelector(DOMstrings.box4).insertAdjacentHTML('beforeend',newHTML);
             //         contador = 0
             // }
-            const newHTML = '<img src="back.svg" alt="">';
-            document.querySelector(DOMstrings.box).insertAdjacentHTML('beforeend',newHTML);           
+                    
                    
         },
         //------------MODAL--------------
         displayModal: (e) => {
             console.log('Entre a displayModal');
-            // let prueba = document.querySelectorAll(DOMstrings.modalContainer)[0];
+            let playerActive = 'Player 1'
+
             document.querySelector(DOMstrings.modalContainer).style.opacity = 1;
             document.querySelector(DOMstrings.modalContainer).style.visibility = 'visible';
             document.querySelector(DOMstrings.modal).classList.toggle('modal-close');
+            // Player Active message         
+            if (document.querySelector(DOMstrings.modalFirstPlayer)) {
+                console.log('existe')
+                const p = document.querySelector(DOMstrings.modalMsg);
+                const c = document.querySelector(DOMstrings.modalFirstPlayer);
+                p.parentNode.removeChild(c);
+            }
+            const newHTML = `<p class="first-player"><span>${playerActive}</span><br>you go first, so tell us...</p>`;
+            document.querySelector(DOMstrings.modalMsg).insertAdjacentHTML('beforebegin',newHTML);
         },
         closeModal: (e) => {
             console.log('Entre a closeModal');
@@ -98,9 +107,9 @@ const UIController = ( () => {
 })();
 
 const controller = ((tictacCtrl, UICtrl) => {
-    const DOM = UICtrl.getDOMstrings();
-    const boxesElement = document.querySelectorAll(DOM.gameContainer);
-    const boxesImage = document.querySelectorAll(DOM.box);
+    const DOM = UICtrl.getDOMstrings(),
+          boxesElement = document.querySelectorAll(DOM.gameContainer),
+          boxesImage = document.querySelectorAll(DOM.box);
     // Event listener
     const setupEventListeners = () =>{
         //--- Boxes click Events----
@@ -114,9 +123,38 @@ const controller = ((tictacCtrl, UICtrl) => {
         document.querySelector(DOM.btnStart).addEventListener('click', ctrlStartGame); //Start-Btn click
         document.querySelector(DOM.btnReset).addEventListener('click', ctrlResetGame); //Reset-Btn click
         //--- Modal Events----
+        document.querySelector(DOM.modalMonkey).addEventListener('click', ctrlPlayerAnimal);
+        document.querySelector(DOM.modalSquirrel).addEventListener('click', ctrlPlayerAnimal)
         document.querySelector(DOM.btnModalStart).addEventListener('click', ctrlModalStart); //Btn-ModalStart
         document.querySelector(DOM.btnModalClose).addEventListener('click', ctrlModalClose); //Btn-ModalClose click
     }
+    //Moldal Control
+    //Btn-ModalStart cick
+    const ctrlModalStart = (e) => {
+        console.log('Modal Start');
+        // 1. Set de values that the player
+        
+        // 2. Close the Modal
+
+    };
+    //Btn-ModalClose click
+    const ctrlModalClose = (e) => {
+        console.log('entra ctrlModalClose');
+        // 1. Reset de values that the player
+        
+        // 2. Close the Modal
+        UICtrl.closeModal();
+    };
+    //Moal-Images click
+    const ctrlPlayerAnimal = (e) => {
+        console.log('entra ctrlPlayerAnimal');
+        // 1. Mirar selecion de Jugador
+        let player
+        // const clickedBox = e.target;
+        e.target.classList.contains('modal-monkey') ?  player = 'monkey' : player = 'squirrel';
+        console.log(`player select: ${player}`);
+
+    };
     //Game Contaner click
     const ctrlGameClick = (e) => {
         // document.querySelector(DOMstrings.).textContent = 'mi madree!'
@@ -129,7 +167,7 @@ const controller = ((tictacCtrl, UICtrl) => {
             if (clickedBoxIndex === 0) {
                
                 document.querySelector('.card').style.transform = "rotateY(180deg)";
-                const newHTML = '<img src="monkey.svg" alt="">';
+                const newHTML = '<img src="squirrel.png" alt="">';
                 document.querySelector('.back').insertAdjacentHTML('beforeend',newHTML); 
             }   
         }
@@ -150,35 +188,19 @@ const controller = ((tictacCtrl, UICtrl) => {
         tictacCtrl.ramdomPlayer()
         // 3. Display de Modal in the UI
         UICtrl.displayModal();
-    };
-    //Btn-ModalStart cick
-    const ctrlModalStart = (e) => {
-        console.log('Modal Start');
-        // 1. Set de values that the player
-        
-        // 2. Close the Modal
-
-    };
-    //Btn-ModalClose click
-    const ctrlModalClose = (e) => {
-        console.log('entra ctrlModalClose');
-        // 1. Reset de values that the player
-        
-        // 2. Close the Modal
-        UICtrl.closeModal();
-    };
-   
+    };   
 
     return {        
         init: () => { 
+            console.log('App has started');
             for (let boxes of boxesImage) {
                 boxes.style.display = 'block';
                 boxes.src = 'monkey.svg';
             }
             tictacCtrl.ramdomPlayer(); //Select a raamdom player
-            // UICtrl.displayModal();
+            UICtrl.displayModal();
             setupEventListeners(); 
-            console.log('App has started');
+            
 
         }
     };
